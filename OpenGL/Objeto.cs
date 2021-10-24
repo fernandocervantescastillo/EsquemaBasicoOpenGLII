f31960e69c96af6a722427d6f44e34ff6602a75a
fernandocervantescastillo <50065506+fernandocervantescastillo@users.noreply.github.com>
2021-10-23 23:51:19 -0400

    Agregar archivos de proyecto.

---
+++ OpenGL/Objeto.cs
@@ -0,0 +1,42 @@
+﻿using OpenGL.common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenGL
+{
+
+    class Objeto
+    {
+        List<Cara> list;
+
+        public Objeto()
+        {
+            list = new List<Cara>();
+        }
+
+        public void addCara(Cara cara)
+        {
+            list.Add(cara);
+        }
+
+        public void render(Camera _camera)
+        {
+            foreach (Cara cara in  list)
+            {
+                cara.render(0, _camera);
+            }
+        }
+
+        public void dispose()
+        {
+            foreach (Cara cara in list)
+            {
+                cara.dispose();
+            }
+
+        }
+
+
+    }
+}
